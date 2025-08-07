f596cc10af85fe48276b98ef0491ede2ffe2f463
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-08-07 22:43:58 +0500

    commited

---
+++ app.js
@@ -0,0 +1,77 @@
+// chapter 1
+
+//alert("Welcome to Js World")
+
+//alert("Error! Please ener a valid password.")
+
+//alert("Hi... I can run Js through my web browser's console.")
+
+ //Chapter 2
+
+ //var username = ("huzaifasyed2627")
+
+ //var myName = ("Syed Huzaifa Tauseef")
+ //var Message = ("Hello World!")
+
+ //alert (Message)
+
+
+ //var fName = ("Fede Valverde")
+
+// alert (fName)
+
+ //var age =("20 years old")
+ //alert(age)
+
+// var accupation =("international footballer")
+// alert(accupation)
+
+
+ //var myPizza = ("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+ //alert (myPizza)
+
+
+// var eMail = ("huzaifasyed2627@gmail.com")
+ //var note = ("My Email Address is ")
+// alert ( note + eMail)
+
+ 
+// var book = ('I am trying to learn from the A Smarter way to Javascript')
+
+//  alert (book)
+
+
+ //var ohYes = ("Yah! I can write HTML content through Javascript")
+
+// document.writeln (ohYes)
+
+
+// var theEnd = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+//alert (theEnd)
+
+
+
+// Chapter 3 
+
+ //var myAge = ("I am 18 years old");
+
+ //alert (myAge)
+
+ //var timeLine = ("you have visited this site 14 times");
+
+
+ //alert (timeLine)
+
+ //var birthYear = ("My birth year is 1947 <br> Data type of my declared variable is strings</br>");
+
+
+
+ //document.writeln (birthYear)
+
+//var visitorName = ("Fede Valverde ordered ");
+//var productTitle =  (" Jersey(s)");
+//var quantity = (19);
+//var store = (" on ABC clothing store");
+
+//document.writeln (visitorName + quantity + productTitle + store);
